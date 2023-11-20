6bc26dd33f7a753dfc93a24c5b97f78c6749d172
natikozel <netanelkozel@gmail.com>
2023-11-21 01:47:21 +0200

    Starting with TypeScript

---
+++ dist/calculator.js
@@ -0,0 +1,73 @@
+"use strict";
+const wrapper = document.querySelector('.wrapper');
+const input = document.querySelector('.screen');
+let total = 0;
+let firstNumber = 0;
+let secondNumber = 0;
+let operator;
+wrapper.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.nodeName === 'BUTTON') {
+        const key = target.textContent.trim();
+        switch (key) {
+            case '←':
+                if (input.textContent.length > 1)
+                    input.textContent = input.textContent.slice(0, input.textContent.length - 1);
+                else
+                    input.textContent = '0';
+                break;
+            case 'C':
+                input.textContent = '0';
+                total = 0;
+                operator = '';
+                firstNumber = 0;
+                secondNumber = 0;
+                break;
+            case '=':
+                const str = input.textContent.split(operator).map(number => number[0] === '−' ? +number.slice(1) * (-1) : +number);
+                if (str.length > 2 && secondNumber === 0)
+                    return;
+                firstNumber = str[0];
+                if (str[1])
+                    secondNumber = str[1];
+                total = handleOperations();
+                break;
+            default:
+                if (input.textContent === '0') {
+                    input.textContent = key;
+                    return;
+                }
+                else if (+key >= 0 && +key <= 9) {
+                    input.textContent += key;
+                    return;
+                }
+                firstNumber = input.textContent[0] === '−' ? +input.textContent.slice(1) * (-1) : +input.textContent;
+                operator = key;
+                input.textContent += key;
+        }
+    }
+});
+const handleOperations = () => {
+    let result;
+    switch (operator) {
+        case '+':
+            result = firstNumber + secondNumber;
+            break;
+        case '×':
+            result = firstNumber * secondNumber;
+            break;
+        case '÷':
+            result = firstNumber / secondNumber;
+            break;
+        case '−':
+            result = firstNumber - secondNumber;
+            break;
+        default:
+            result = null;
+    }
+    if (!result)
+        return result;
+    input.textContent = '' + result;
+    firstNumber = result;
+    return result;
+};
---
+++ src/calculator.ts
@@ -0,0 +1,80 @@
+const wrapper = document.querySelector('.wrapper')! as HTMLDivElement;
+const input = document.querySelector('.screen')! as HTMLInputElement;
+let total: number | null = 0;
+let firstNumber: number = 0
+let secondNumber: number = 0;
+let operator: string;
+
+wrapper.addEventListener('click', (event: MouseEvent): void => {
+
+    const target = event.target as HTMLButtonElement
+    if (target.nodeName === 'BUTTON') {
+        const key: string = target.textContent!.trim()
+
+        switch (key) {
+            case '←':
+                if (input.textContent!.length > 1)
+                    input.textContent = input.textContent!.slice(0, input.textContent!.length - 1)
+                else
+                    input.textContent = '0'
+                break;
+            case 'C':
+                input.textContent = '0';
+                total = 0;
+                operator = '';
+                firstNumber = 0
+                secondNumber = 0;
+
+                break;
+            case '=':
+                const str: Array<number> = input.textContent!.split(operator).map(number => number[0] === '−' ? +number.slice(1) * (-1) : +number)
+                if (str.length > 2 && secondNumber === 0)
+                    return
+                firstNumber = str[0]
+                if (str[1])
+                    secondNumber = str[1]
+                total = handleOperations();
+                break;
+
+            default:
+                if (input.textContent === '0') {
+                    input.textContent = key
+                    return
+                } else if (+key >= 0 && +key <= 9) {
+                    input.textContent += key
+                    return
+                }
+
+                firstNumber = input.textContent![0] === '−' ? +input.textContent!.slice(1) * (-1) : +input.textContent!
+                operator = key
+                input.textContent += key
+
+        }
+    }
+})
+
+
+const handleOperations = (): number | null => {
+    let result: number | null
+    switch (operator) {
+        case '+':
+            result = firstNumber + secondNumber
+            break;
+        case '×':
+            result = firstNumber * secondNumber
+            break;
+        case '÷':
+            result = firstNumber / secondNumber
+            break;
+        case '−':
+            result = firstNumber - secondNumber
+            break;
+        default:
+            result = null;
+    }
+    if (!result)
+        return result
+    input.textContent = '' + result
+    firstNumber = result
+    return result
+}
